0438e9a0df13f0c0f71c62d420b6648b3419ef8f
Toru Oshima <selfmadetool0213@icloud.com>
2019-08-10 21:33:33 +0900

    Create artist-destroy function

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,11 +1,16 @@
 class ArtistsController < ApplicationController
-  before_action :set_artist, only: [:show]
+  before_action :set_artist, only: [:show, :destroy]
 
   def index
     @artists = Artist.all
   end
 
   def show
+  end
+
+  def destroy
+    @artist.destroy if user_signed_in?
+    redirect_to artists_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'articles#index'
   resources :users, only: [:index, :edit, :update]
-  resources :artists, only: [:index, :show]
+  resources :artists, only: [:index, :show, :update, :destroy]
 end
