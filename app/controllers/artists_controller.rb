d81a321255cf6247eb836cc64324d5bab6c0398b
Toru Oshima <selfmadetool0213@icloud.com>
2019-08-11 16:30:58 +0900

    Create artist-edit function

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,5 @@
 class ArtistsController < ApplicationController
-  before_action :set_artist, only: [:show, :destroy]
+  before_action :set_artist, only: [:show, :edit, :destroy]
 
   def index
     @artists = Artist.all
@@ -8,12 +8,25 @@ class ArtistsController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    artist = Artist.find(params[:id])
+    artist.update(artist_params) if user_signed_in?
+    redirect_to artist_path(artist.id)
+  end
+
   def destroy
     @artist.destroy if user_signed_in?
     redirect_to artists_path
   end
 
   private
+
+  def artist_params
+    params.require(:artist).permit(:name, :image, :text)
+  end
 
   def set_artist
     @artist = Artist.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'articles#index'
   resources :users, only: [:index, :edit, :update]
-  resources :artists, only: [:index, :show, :update, :destroy]
+  resources :artists, only: [:index, :show, :edit, :update, :destroy]
 end
