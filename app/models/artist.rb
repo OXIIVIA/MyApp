ddc79ab4665751f2769be3a49369c73908441367
Toru Oshima <selfmadetool0213@icloud.com>
2019-08-12 16:21:56 +0900

    Create movies-index page

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,5 @@
 class Artist < ApplicationRecord
   has_many :follow_users, dependent: :destroy
   has_many :users, through: :follow_users
+  has_many :videos
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   devise_for :users
   root 'articles#index'
   resources :users, only: [:index, :edit, :update]
   resources :artists, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :videos, only: [:index, :show]
 
   post '/follow_users/:artist_id' => 'follow_users#create', as: 'follow'
   delete '/follow_users/:artist_id' => 'follow_users#destroy', as: 'unfollow'
