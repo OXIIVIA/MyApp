db408f52e67e28ec593e10a22b32661c6bd3a21a
Toru Oshima <selfmadetool0213@icloud.com>
2019-08-12 13:22:59 +0900

    Create video model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190811153603) do
+ActiveRecord::Schema.define(version: 20190812041304) do
 
   create_table "artists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                     null: false
@@ -44,6 +44,16 @@ ActiveRecord::Schema.define(version: 20190811153603) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "videos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "title",      null: false
+    t.string   "videoID",    null: false
+    t.integer  "artist_id",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_videos_on_artist_id", using: :btree
+  end
+
   add_foreign_key "follow_users", "artists"
   add_foreign_key "follow_users", "users"
+  add_foreign_key "videos", "artists"
 end
