77687d0c7810585a699b208b5038e8b3c109bdb1
Toru Oshima <selfmadetool0213@icloud.com>
2019-08-13 23:40:18 +0900

    Update article-index view

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :edit, :update]
   resources :artists, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :videos, only: [:index, :show]
+  resources :article, only: [:index, :show]
 
   post '/follow_users/:artist_id' => 'follow_users#create', as: 'follow'
   delete '/follow_users/:artist_id' => 'follow_users#destroy', as: 'unfollow'
