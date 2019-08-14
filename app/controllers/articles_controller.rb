2ec85fca2de7d397c5cebe4440996ca17d85889b
Toru Oshima <selfmadetool0213@icloud.com>
2019-08-14 21:33:07 +0900

    Create article-show function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,22 @@
 class ArticlesController < ApplicationController 
+  before_action :set_article, only: [:show]
+  before_action :set_artist, only: [:show]
+
   def index
     @articles = Article.all
+  end
+
+  def show
+    @articles = @artist.articles.where.not(id: @article.id)
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def set_artist
+    @artist = Article.find(params[:id]).artist
   end
 end
