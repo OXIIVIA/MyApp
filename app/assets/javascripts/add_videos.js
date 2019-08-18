0c8dcbb5e865c88bbc5aa4bff3f5ff90c64b5515
Toru Oshima <selfmadetool0213@icloud.com>
2019-08-18 14:24:25 +0900

    Create add_videos.js

---
+++ app/assets/javascripts/add_videos.js
@@ -0,0 +1,14 @@
+$(document).on('turbolinks:load', function() {
+
+
+  $('.videos__button-box__more-btn').on('click', function() {
+    last_video_id = $('.video').last().attr('data-id');
+    url = window.location.href;
+    $.ajax({
+      url: "url",
+      type: 'GET',
+      dataType: 'json',
+      data: {id: last_video_id}
+    })
+  });
+});
