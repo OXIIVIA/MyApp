f5866dcd560d1e4d907430d23790bac174e561b0
Toru Oshima <selfmadetool0213@icloud.com>
2019-08-18 14:55:37 +0900

    Update add_videos.js

--- app/assets/javascripts/add_videos.js
+++ app/assets/javascripts/add_videos.js
@@ -1,5 +1,21 @@
 $(document).on('turbolinks:load', function() {
+  
+  function addVideo(video) {
+    var html = `<a class="video" data-id="${video.id}" href="/videos/${video.id}">
+                  <div class="video__inner">
+                    <div class="video__inner__thumnail">
+                      <img src="http://img.youtube.com/vi/${video.videoID}/mqdefault.jpg" alt="Mqdefault">
+                    </div>
+                    <div class="video__inner__details">
+                      <p class="video__inner__details__song-title">${video.title}</p>
+                      <p class="video__inner__details__artist-name">${video.artist}</p>
+                      <p class="video__inner__details__date">${video.created_at}</p>
+                    </div>
+                  </div>
+                </a>`
 
+    $('.videos__videos-box__wrapper').append(html);
+  }
 
   $('.videos__button-box__more-btn').on('click', function() {
     last_video_id = $('.video').last().attr('data-id');
@@ -10,5 +26,14 @@ $(document).on('turbolinks:load', function() {
       dataType: 'json',
       data: {id: last_video_id}
     })
+    .done(function(videos) {
+      var reversedVideos = videos.reverse();
+      $.each(reversedVideos, function(i, video) {
+        addVideo(video);
+      });
+    })
+    .fail(function() {
+      console.log("error")
+    });
   });
 });
--- app/views/videos/index.json.jbuilder
+++ app/views/videos/index.json.jbuilder
@@ -2,6 +2,6 @@ json.array! @next do |video|
   json.id video.id
   json.title video.title
   json.videoID video.videoID
-  json.artist_id video.artist_id
+  json.artist video.artist.name
   json.created_at video.created_at.strftime("%Y/%m/%d")
 end
