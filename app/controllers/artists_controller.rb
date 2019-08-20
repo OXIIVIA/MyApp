70597f201c6ed534c468bb375fc063b2e80dc688
Toru Oshima <selfmadetool0213@icloud.com>
2019-08-20 15:10:49 +0900

    delete routes in artist page

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,5 @@
 class ArtistsController < ApplicationController
-  before_action :set_artist, only: [:show, :edit, :destroy]
+  before_action :set_artist, only: :show
 
   def index
     @artists = Artist.order(:name)
@@ -9,35 +9,10 @@ class ArtistsController < ApplicationController
     @artist = Artist.new
   end
 
-  def create
-    artist = Artist.new(artist_params)
-    artist.save if current_user.admin_flg?
-    redirect_to artists_path
-  end
-
   def show
-
-  end
-
-  def edit
-  end
-
-  def update
-    artist = Artist.find(params[:id])
-    artist.update(artist_params) if current_user.admin_flg?
-    redirect_to artist_path(artist.id)
-  end
-
-  def destroy
-    @artist.destroy if current_user.admin_flg?
-    redirect_to artists_path
   end
 
   private
-
-  def artist_params
-    params.require(:artist).permit(:name, :image, :text)
-  end
 
   def set_artist
     @artist = Artist.find(params[:id])
